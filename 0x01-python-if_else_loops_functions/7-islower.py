f3b42fe8c116b5c2464a66c94e1b7641620a4634
LMI-crypto <bettykamau333@gmail.com>
2022-05-31 22:46:02 +0300

    new file

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
-    if c > ord('a') and c <= ord('z'):
-        return(True)
-    
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
     else:
-        return(False)
-    
+        return False
